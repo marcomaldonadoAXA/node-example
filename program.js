58a3f0cfa5ed4115e816c3b9359d816395f43f05
Marco Maldonado <loweaxerium@gmail.com>
2017-01-04 11:25:36 -0600

    Juggling Async

--- program.js
+++ program.js
@@ -1,13 +1,25 @@
 var http = require('http');
 var bl = require('bl');
+var results = ["", "", ""];
+var count = 0;
+for (var i = 0; i < 3; i++) {
+  get(i);
+}
 
-http.get(process.argv[2], function callback(response) {
-  response.setEncoding("utf8");
-  response.pipe(bl(function (err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log(data.length);
-    console.log(data.toString());
-  }))
-}).on("error",console.error);
+function get(index) {
+  http.get(process.argv[index + 2], function callback(response) {
+    response.setEncoding("utf8");
+    response.pipe(bl(function (err, data) {
+      if (err) {
+        return callback(err);
+      }
+      results[index] = data.toString();
+      count++;
+      if (count === 3) {
+        results.forEach(function (result) {
+          console.log(result);
+        })
+      }
+    }))
+  }).on("error",console.error);
+};
