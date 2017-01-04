3e6806cd3666cf52772d38e1048b1a768b986af9
Marco Maldonado <loweaxerium@gmail.com>
2017-01-04 10:32:34 -0600

    Http Collect

--- program.js
+++ program.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var bl = require('bl');
 
 http.get(process.argv[2], function callback(response) {
   response.setEncoding("utf8");
-  response.on("data", console.log);
-  response.on("error", console.error);
+  response.pipe(bl(function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(data.length);
+    console.log(data.toString());
+  }))
 }).on("error",console.error);
