938d096d07858edda7a1df6bdf90f9b77bc78ace
Marco Maldonado <loweaxerium@gmail.com>
2017-01-04 12:02:31 -0600

    Http File Server

--- program.js
+++ program.js
@@ -1,6 +1,9 @@
-var net = require('net')
-var strftime = require('strftime');
-var server = net.createServer(function (socket) {
-  socket.end(strftime('%F %R\n', new Date()));
-});
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  var src = fs.createReadStream(process.argv[3])
+  src.pipe(res)
+})
 server.listen(process.argv[2])
